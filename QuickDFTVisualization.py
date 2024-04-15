5963fdd9be23efb14619eba992e9bbf149c2ce12
ibanrohazz <itr5720@g.rit.edu>
2024-04-14 23:17:28 -0400

    cleaned up file for finished ?

--- QuickDFTVisualization.py
+++ QuickDFTVisualization.py
@@ -31,24 +31,39 @@ def plotDFT(data):
     plt.show()
     
 # Question 2 Data 
-# data = np.array([1/3, 1/3, 0, 0, 0, 0, 0, 1/3]) #2a
-# data = np.array([1/3, 1/3, 1/3, 0, 0, 0, 0, 0]) #2b
-# data = np.array([1, 1/(2**.5), 0, -1/(2**.5), -1, -1/(2**.5), 0, 1/(2**.5)]) #2c
-# data = np.array([1, -1, 1, -1, 1, -1, 1, -1]) #2d
-# data = np.array([1, 1, 1, 1, -1, -1, -1, -1]) #2e
+def question2():
+    data = np.array([1/3, 1/3, 0, 0, 0, 0, 0, 1/3]) #2a
+    # data = np.array([1/3, 1/3, 1/3, 0, 0, 0, 0, 0]) #2b
+    # data = np.array([1, 1/(2**.5), 0, -1/(2**.5), -1, -1/(2**.5), 0, 1/(2**.5)]) #2c
+    # data = np.array([1, -1, 1, -1, 1, -1, 1, -1]) #2d
+    # data = np.array([1, 1, 1, 1, -1, -1, -1, -1]) #2e
+    
+    plotDFT(data)
+    print(DFT(data))
 
 # Question 3 Data
-# a 256 samples array ie one cycle of the cosine in the array 
-# data = np.cos(np.linspace(0, 2*np.pi, 256))
-# a 64 samples array ie four cycles of the cosine in the array
-# data = np.cos(np.linspace(0, 8*np.pi, 64))
-# a sine with period equal to 64 
-# data = np.sin(np.linspace(0, 2*np.pi, 64))
-# a 64 samples cos array with a phase shift of pi/4
-# data = np.cos(np.linspace(0, 8*np.pi, 64) + np.pi/4)
-# a cosine with a period of 16 samples 
-data = np.cos(np.linspace(0, 2*np.pi, 16))
+def question3():
+    # a 256 samples array ie one cycle of the cosine in the array 
+    # data = np.cos(np.linspace(0, 2*np.pi, 256))
+    # a 64 samples array ie four cycles of the cosine in the array
+    # data = np.cos(np.linspace(0, 8*np.pi, 64))
+    # a sine with period equal to 64 
+    # data = np.sin(np.linspace(0, 2*np.pi, 64))
+    # a 64 samples cos array with a phase shift of pi/4
+    # data = np.cos(np.linspace(0, 8*np.pi, 64) + np.pi/4)
+    # a cosine with a period of 16 samples 
+    data = np.cos(np.linspace(0, 2*np.pi, 16))
+    
+    plotDFT(data)
+    print(DFT(data))
 
-dft = DFT(data)
-plotDFT(data)
-print(dft)
+def run():
+    data = input("Enter data: ")
+    data = np.array(data)
+    plotDFT(data)
+    print(DFT(data))
+    
+if  __name__ == '__main__':
+    # run()
+    # question2()
+    question3()
